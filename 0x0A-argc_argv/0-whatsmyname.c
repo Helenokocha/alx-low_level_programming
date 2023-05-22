9e9b2bff5be9994c49bee8e7088842289aec8b1f
helenokocha <okochahelen7@gmail.com>
2023-05-22 10:11:13 -0700

    3-mul.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc__attribute__,((unused)), char *argv[])
+
+int main(int argc__attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
